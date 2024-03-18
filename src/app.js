c62b475f61d07423884d51a653b8dd82bc53d4d1
vasujain275 <vasujain275@gmail.com>
2024-03-18 13:57:58 +0530

    Established Connection to DB

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
-  }),
+  })
 );
 
 app.use(express.json({ limit: "16kb" }));
--- src/db/connetDB.js
+++ src/db/connetDB.js
@@ -4,10 +4,10 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`,
+      `${process.env.MONGODB_URI}/${DB_NAME}`
     );
     console.log(
-      `MongoDB Instance Connected! DB Host : ${connectionInstance.connection.host}`,
+      `MongoDB Instance Connected! DB Host : ${connectionInstance.connection.host}`
     );
   } catch (error) {
     console.log("DB Connection Error ", error);
