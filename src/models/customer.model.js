6a6e42278fd3179465b22cb3e415e26bbc7f2e0a
vasujain275 <vasujain275@gmail.com>
2024-03-18 14:36:16 +0530

    Added Customer and Order Models

---
+++ src/models/customer.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const customerSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    reqired: false
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    require: false
+  },
+  orders: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order"
+    }
+  ]
+}, { timestamps: true });
+
+export const Customer = new mongoose.model("Customer", customerSchema);
---
+++ src/models/order.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const orderStatusEnum = {
+  values: ['Pending', 'Processing', 'Shipped', 'Delivered', 'Cancelled'],
+  message: '{VALUE} is not a valid order status'
+};
+
+
+const orderSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product"
+  },
+  customer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Customer"
+  },
+  destination: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: orderStatusEnum,
+    default: "Pending"
+  },
+  warehouse: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Warehouse"
+  }
+}, { timestamps: true });
+
+export const Order = new mongoose.model("Order", orderSchema);
